f98a912b74f34833077581809e89881412174cdd
arian <arian.collaso.rodriguez@gmail.com>
2022-01-18 21:14:09 +0100

    Switch Them Dark Complete

--- src/components/botonDarkTheme.js
+++ src/components/botonDarkTheme.js
@@ -2,29 +2,34 @@ import React from "react";
 import '../styles/botonDarktheme.css';
 import logoLight from '../images/light.png';
 import logoDark from '../images/dark.png';
-import { click } from "@testing-library/user-event/dist/click";
+
 
 // Falta crear la funcion que cambie el estado ::after y le de la clase active
 // Falta tambien que se cambie el body a oscuro.
 
 
-class BotonDarkTheme extends React.Component{
+const BotonDarkTheme = () => {
 
-    render(){
-        return(
-            
-            <button /*>onClick={this.btnSwitch}*/ className="switch" id="switch">
-                <img className="logoLight" src={logoLight}/>
-                <img className="logoDark" src={logoDark}/>
-            </button>
-            
-        )
-    } 
+        const btnSwitch = ()=> {
+            const elemento = document.getElementById("switch");
+            const elementoCuerpo = document.getElementById("cuerpo");
+            if(elemento.className == "switch" && elementoCuerpo.className == "cuerpo"){
+                elemento.className +=" active";
+                elementoCuerpo.className +=" active";}
+            else{
+                elemento.classList.remove("active");
+            elementoCuerpo.classList.remove("active");}
+
+        }
+
+    
+    return(
+        <button onClick={()=> btnSwitch()} className="switch" id="switch">
+            <img className="logoLight" src={logoLight}/>
+            <img className="logoDark" src={logoDark}/>
+        </button>
+    )
+ 
 }
-
-
-
-
-
 
 export default BotonDarkTheme;
