2f2f2880491c78f3b2e7f761a6e88edad5ac0b7e
arian <arian.collaso.rodriguez@gmail.com>
2022-01-18 19:39:14 +0100

    el switch active en la consola de desarrollo funciona pero falta implementar

--- src/components/botonDarkTheme.js
+++ src/components/botonDarkTheme.js
@@ -1,23 +1,28 @@
 import React from "react";
 import '../styles/botonDarktheme.css';
 import logoLight from '../images/light.png';
-import logoDark from '../images/dark.png'
+import logoDark from '../images/dark.png';
+import { click } from "@testing-library/user-event/dist/click";
+
+// Falta crear la funcion que cambie el estado ::after y le de la clase active
+// Falta tambien que se cambie el body a oscuro.
+
 
 class BotonDarkTheme extends React.Component{
+
     render(){
         return(
-            <button className="switch" id="switch">
+            
+            <button /*>onClick={this.btnSwitch}*/ className="switch" id="switch">
                 <img className="logoLight" src={logoLight}/>
                 <img className="logoDark" src={logoDark}/>
             </button>
+            
         )
     } 
-
 }
-const btnSwitch = document.getElementById('switch');
-btnSwitch.addEventListener('click', ()=>{
-    document.BotonDarkTheme.classList.toggle('logoDark');
-});
+
+
 
 
 
